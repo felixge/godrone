b6d25e07e1aff3633e850575143d174b97e3b8ea
Felix Geisendörfer <felix@debuggable.com>
2013-03-16 00:47:58 +0100

    Better

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,6 +3,7 @@ set -eu
 
 root_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
 drone_ip="192.168.1.1"
+bin_name="godrone"
 
 echo "--> Compiling arm binary ..."
 env \
@@ -12,7 +13,7 @@ env \
   go install github.com/felixge/godrone/src/cmd/godrone
 
 echo "--> Uploading via ftp ..."
-curl -T "${root_dir}/gopath/bin/linux_arm/godrone" "ftp://@${drone_ip}/upload"
+curl -T "${root_dir}/gopath/bin/linux_arm/${bin_name}" "ftp://@${drone_ip}/${bin_name}.next"
 
 echo "--> Starting godrone ..."
-"${root_dir}/scripts/start.expect"
+"${root_dir}/scripts/start.expect" "${drone_ip}" "${bin_name}"
