786f6f7d33eebf95ee08c600ab943e6ead29d785
Felix Geisendörfer <felix@debuggable.com>
2013-03-23 19:07:22 +0100

    Support deploying different binaries

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,14 +3,14 @@ set -eu
 
 root_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
 drone_ip="192.168.1.1"
-bin_name="godrone"
+bin_name="${1:-godrone}"
 
 echo "--> Compiling arm binary ..."
 env \
   GOOS=linux \
   GOARCH=arm \
   CGO_ENABLED=0 \
-  go install github.com/felixge/godrone/src/cmd/godrone
+  go install "github.com/felixge/godrone/src/cmd/${bin_name}"
 
 echo "--> Uploading via ftp ..."
 curl -T "${root_dir}/gopath/bin/linux_arm/${bin_name}" "ftp://@${drone_ip}/${bin_name}.next"
