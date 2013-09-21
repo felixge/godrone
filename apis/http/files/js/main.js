cb59b9cf628f7ba56090d12b449c330247465e11
Felix Geisendörfer <felix@debuggable.com>
2013-09-21 17:29:49 +0200

    Pretty

--- apis/http/files/js/main.js
+++ apis/http/files/js/main.js
@@ -21,82 +21,82 @@ $(function() {
   }, 1000);
 
   //
-		// We use an inline data source in the example, usually data would
-		// be fetched from a server
+  // We use an inline data source in the example, usually data would
+  // be fetched from a server
 
-		var totalPoints = 300, scale = 100;
-    var fields = {};
+  var totalPoints = 300, scale = 100;
+  var fields = {};
 
-		function getNavdata(fieldName) {
-      var val = (navdata)
-        ? navdata[fieldName]
-        : 0;
+  function getNavdata(fieldName) {
+    var val = (navdata)
+      ? navdata[fieldName]
+      : 0;
 
-      var field = fields[fieldName];
-      if (!field) {
-        field = fields[fieldName] = {min: 0, max: 0, data: []}
-      }
-
-      if (val < field.min) {
-        field.min = val;
-      }
-      if (val > field.max) {
-        field.max = val;
-      }
-
-      if (field.data.length == 0) {
-        for (var i = 0; i < totalPoints; i++) {
-          field.data.push(0);
-        }
-      }
-
-      scaled = ((val - field.min) / (field.max - field.min)) * scale;
-
-      field.data.shift();
-      field.data.push(scaled);
-
-			// Zip the generated y values with the x values
-
-			var res = [];
-			for (var i = 0; i < field.data.length; ++i) {
-				res.push([i, field.data[i]])
-			}
-
-			return res;
-		}
-
-    function getPlots() {
-      return [
-        getNavdata('Gx'),
-        getNavdata('Gy'),
-        getNavdata('Gz'),
-        getNavdata('Ax'),
-        getNavdata('Ay'),
-        getNavdata('Az'),
-      ];
+    var field = fields[fieldName];
+    if (!field) {
+      field = fields[fieldName] = {min: 0, max: 0, data: []}
     }
 
-		var plot = $.plot("#placeholder", getPlots(), {
-			series: {
-				shadowSize: 0,
-			},
-			yaxis: {
-				min: 0,
-				max: scale
-			},
-			xaxis: {
-				show: false
-			}
-		});
+    if (val < field.min) {
+      field.min = val;
+    }
+    if (val > field.max) {
+      field.max = val;
+    }
 
-		function update() {
-			plot.setData(getPlots());
+    if (field.data.length == 0) {
+      for (var i = 0; i < totalPoints; i++) {
+        field.data.push(0);
+      }
+    }
 
-			// Since the axes don't change, we don't need to call plot.setupGrid()
+    scaled = ((val - field.min) / (field.max - field.min)) * scale;
 
-			plot.draw();
-			requestAnimationFrame(update);
-		}
+    field.data.shift();
+    field.data.push(scaled);
 
-		update();
+    // Zip the generated y values with the x values
+
+    var res = [];
+    for (var i = 0; i < field.data.length; ++i) {
+      res.push([i, field.data[i]])
+    }
+
+    return res;
+  }
+
+  function getPlots() {
+    return [
+      getNavdata('Gx'),
+    getNavdata('Gy'),
+    getNavdata('Gz'),
+    getNavdata('Ax'),
+    getNavdata('Ay'),
+    getNavdata('Az'),
+    ];
+  }
+
+  var plot = $.plot("#plot", getPlots(), {
+    series: {
+      shadowSize: 0,
+    },
+      yaxis: {
+        min: 0,
+      max: scale
+      },
+      xaxis: {
+        show: false
+      }
+  });
+
+  function update() {
+    plot.setData(getPlots());
+
+    // Since the axes don't change, we don't need to call plot.setupGrid()
+
+    plot.draw();
+    requestAnimationFrame(update);
+  }
+
+  update();
 });
