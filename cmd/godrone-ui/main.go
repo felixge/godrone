2ea70a24c29c7802eee3eb7114c1b4ef00355f0d
Felix Geisendörfer <felix@debuggable.com>
2014-10-10 11:40:10 +0200

    UI!

---
+++ cmd/godrone-ui/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"flag"
+	"log"
+)
+import "net/http"
+
+import (
+	"path/filepath"
+	"runtime"
+)
+
+func main() {
+	log.SetFlags(log.Ldate | log.Ltime | log.Lmicroseconds)
+	var addr = flag.String("addr", ":8080", "The addr to listen on.")
+	flag.Parse()
+	dir := flag.Arg(0)
+	if dir == "" {
+		_, srcFile, _, _ := runtime.Caller(0)
+		dir = filepath.Join(filepath.Dir(srcFile), "public")
+	}
+	log.Printf("Listening on: %s", *addr)
+	http.ListenAndServe(*addr, http.FileServer(http.Dir(dir)))
+}
---
+++ cmd/godrone-ui/public/main.js
@@ -0,0 +1,203 @@
+"use strict";
+
+var minAltitude = 0.3;
+var pause = false;
+var emergency = false;
+var chartIndex = 0;
+var colors = ['red', 'black'];
+var desired = {pitch: 0, roll: 0, yaw: 0, altitude: 0};
+var charts = [
+  {
+    title: 'Altitude',
+    labels: ['Time', 'Altitude', 'Desired'],
+    data: [],
+    colors: colors,
+  },
+  {
+    title: 'Pitch',
+    labels: ['Time', 'Actual', 'Desired'],
+    data: [],
+    valueRange: [-90, 90],
+    colors: colors,
+  },
+  {
+    title: 'Roll',
+    labels: ['Time', 'Actual', 'Desired'],
+    data: [],
+    valueRange: [-90, 90],
+    colors: colors,
+  },
+  {
+    title: 'Yaw',
+    labels: ['Time', 'Actual', 'Desired'],
+    data: [],
+    valueRange: [-90, 90],
+    colors: colors,
+  },
+];
+
+var chartData = [];
+var dygraph = new Dygraph(document.getElementById('chart'), charts[chartIndex].data, {
+  drawPoints: true,
+  showRoller: true,
+  labels: charts[chartIndex].labels,
+  valueRange: charts[chartIndex].valueRange,
+  title: charts[chartIndex].title,
+  colors: charts[chartIndex].colors,
+})
+
+function Conn(options) {
+  var ws = new WebSocket(options.url);
+  var reconnect = function() {
+    ws.onopen = function() {};
+    ws.onmessage = function() {};
+    ws.onerror = function() {};
+    reconnect = function() {};
+    ws.close();
+    setTimeout(function() {
+      new Conn(options);
+    }, 1000);
+  };
+  var lastSend;
+
+  ws.onerror = function(e) {
+    console.log('Failed to connect to: '+options.url, e);
+    reconnect();
+  };
+  ws.onopen = function() {
+    console.log('Connected to: '+options.url);
+    ws.send(JSON.stringify({}));
+    lastSend = Date.now();
+  };
+  ws.onmessage = function(e) {
+    try {
+      var data = JSON.parse(e.data);
+    } catch (err) {
+      console.log('Received bad ws data', err);
+      reconnect();
+      return;
+    }
+    var time = new Date(data.time);
+    charts[0].data.push([
+      time,
+      data.actual.Altitude,
+      data.desired.Altitude,
+    ]);
+    charts[1].data.push([
+      time,
+      data.actual.Pitch,
+      data.desired.Pitch,
+    ]);
+    charts[2].data.push([
+      time,
+      data.actual.Roll,
+      data.desired.Roll,
+    ]);
+    charts[3].data.push([
+      time,
+      data.actual.Yaw,
+      data.desired.Yaw,
+    ]);
+    if (!pause) {
+      var chart = charts[chartIndex];
+      dygraph.updateOptions({file: chart.data, dateWindow: [time-10000, time]});
+    }
+
+    var latency = Date.now() - lastSend;
+    var timeout = Math.max(0, 1000/options.hz-latency);
+    lastSend = Date.now();
+    setTimeout(function() {
+      var msg = {setDesired: desired};
+      if (emergency) {
+        desired = {pitch: 0, roll: 0, yaw: 0, altitude: 0};
+      } else {
+        var altSpeed = 0.5/options.hz;
+        var yawSpeed = 10/options.hz;
+        var speed = 3;
+        if (isDown[KEYS.w]) {
+          desired.pitch = -speed;
+        } else if (isDown[KEYS.s]) {
+          desired.pitch = speed;
+        } else {
+          desired.pitch = 0;
+        }
+        if (isDown[KEYS.a]) {
+          desired.roll = -speed;
+        } else if (isDown[KEYS.d]) {
+          desired.roll = speed;
+        } else {
+          desired.roll = 0;
+        }
+        if (isDown[KEYS.left]) {
+          desired.yaw += speed;
+        } else if (isDown[KEYS.right]) {
+          desired.yaw -= speed;
+        }
+        if (isDown[KEYS.up]) {
+          desired.altitude += altSpeed;
+          desired.altitude = Math.max(desired.altitude, minAltitude);
+        } else if (isDown[KEYS.down]) {
+          desired.altitude -= altSpeed;
+          if (desired.altitude < minAltitude) {
+            desired.altitude = 0;
+          }
+        }
+        if (isDown[KEYS.c]) {
+          msg.calibrate = true;
+        }
+      }
+      ws.send(JSON.stringify(msg));
+    }, timeout);
+  };
+}
+
+new Conn({
+  url: 'ws://192.168.1.1',
+  hz: 30,
+});
+
+var KEYS = {
+  esc: 27,
+  left: 37,
+  up: 38,
+  right: 39,
+  down: 40,
+  c: 67,
+  w: 87,
+  s: 83,
+  a: 65,
+  d: 68,
+  p: 80,
+  x: 88,
+};
+var isDown = {};
+
+window.onkeydown = function(e) {
+  console.log(e.keyCode);
+  isDown[e.keyCode] = true;
+  switch (e.keyCode) {
+    case KEYS.esc:
+      emergency = true;
+      break;
+    case KEYS.p:
+      pause = !pause;
+      break;
+    case KEYS.x:
+      chartIndex++;
+      if (chartIndex >= charts.length) {
+        chartIndex = 0;
+      }
+      var chart = charts[chartIndex];
+      dygraph.updateOptions({
+        file: chart.data,
+        labels: chart.labels,
+        valueRange: chart.valueRange,
+        title: chart.title,
+      });
+      break;
+  }
+}
+
+window.onkeyup = function(e) {
+  isDown[e.keyCode] = false;
+}
